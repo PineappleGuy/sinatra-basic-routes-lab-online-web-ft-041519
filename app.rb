2e3187f0551560c8ec5867dd5f442d88ae704b7d
PineappleGuy <garff247@gmail.com>
2019-06-20 19:11:47 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Garth"
+    end
+
+    get '/hometown' do
+        "My hometown is Cheyenne, Wyoming"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'You Need To Calm Down' By Taylor Swift"
+    end
+
 end
